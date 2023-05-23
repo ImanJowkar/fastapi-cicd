b365177245a22ab447fcff2ee8fa704fd4256921
imanjowkar <imanjowkar99@gmail.com>
2023-05-24 00:42:15 +0330

    rearrange the project, and makes it ready for cicd demo for recording vedio

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
-from dotenv import dotenv_values
 from pymongo import MongoClient
 from routes import router as book_router
+import os
 
-config = dotenv_values(".env")
+
+db_url = os.environ["db_url"]
+db_name = os.environ["db_name"]
 
 app = FastAPI()
 
 @app.on_event("startup")
 def startup_db_client():
-    app.mongodb_client = MongoClient(config["ATLAS_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
+    app.mongodb_client = MongoClient(db_url)
+    app.database = app.mongodb_client[db_name]
 
 @app.on_event("shutdown")
 def shutdown_db_client():
